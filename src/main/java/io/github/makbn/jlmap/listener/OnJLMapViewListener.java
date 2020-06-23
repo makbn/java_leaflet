95db2afac84cb7278b2b67965cf257f233d8435d
makbn <mehdi74akbarian@gmail.com>
2020-06-24 03:45:58 +0430

    listeners updated

--- src/main/java/io/github/makbn/jlmap/listener/OnJLMapViewListener.java
+++ src/main/java/io/github/makbn/jlmap/listener/OnJLMapViewListener.java
@@ -1,17 +1,74 @@
 package io.github.makbn.jlmap.listener;
 
 import io.github.makbn.jlmap.JLMapView;
+import io.github.makbn.jlmap.model.JLBounds;
+import io.github.makbn.jlmap.model.JLLatLng;
 
-public interface OnJLMapViewListener {
+
+public abstract class OnJLMapViewListener {
 
     /**
      * called after the map is fully loaded
+     *
      * @param mapView
      */
-    void mapLoadedSuccessfully(JLMapView mapView);
+    public abstract void mapLoadedSuccessfully(JLMapView mapView);
 
     /**
      * called after the map got an exception on loading
      */
-    void mapFailed();
+    public abstract void mapFailed();
+
+    /**
+     * Fired repeatedly during any movement of the map, including pan and fly animations.
+     *
+     * @param action    action of movement
+     * @param center    coordinate of map
+     * @param bounds    of map
+     * @param zoomLevel of map
+     */
+    public void onMove(Action action, JLLatLng center, JLBounds bounds, int zoomLevel) {
+
+    }
+
+    /**
+     * Fired repeatedly during any movement of the map, including pan and fly animations.
+     * Fired when the map has changed, after any animations.
+     * Fired when the center of the map stats/stops changing (e.g. user starts/stopped dragging the map).
+     *
+     * @param action zoom action {{@link Action#ZOOM}}
+     *               or {{@link Action#ZOOM_START}} or {{@link Action#ZOOM_END}}
+     */
+    public void onZoom(Action action, int zoomLevel) {
+
+    }
+
+    public enum Action {
+        /**
+         * zoom level changes continuously
+         */
+        ZOOM,
+        /**
+         * zoom level stats to change
+         */
+        ZOOM_START,
+        /**
+         * zoom leve changes end
+         */
+        ZOOM_END,
+
+        /**
+         * map center changes continuously
+         */
+        MOVE,
+        /**
+         * user starts to move the map
+         */
+        MOVE_START,
+        /**
+         * user ends to move the map
+         */
+        MOVE_END
+
+    }
 }
