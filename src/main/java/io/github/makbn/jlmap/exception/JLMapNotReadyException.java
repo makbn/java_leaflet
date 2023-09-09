7f4930e7c4d044b5fa0eddf6618a53a779423bde
Matt <mehdi.rastaghi@aimsio.com>
2023-09-09 03:38:17 -0600

    adding a constant for exception message

--- src/main/java/io/github/makbn/jlmap/exception/JLMapNotReadyException.java
+++ src/main/java/io/github/makbn/jlmap/exception/JLMapNotReadyException.java
@@ -6,12 +6,13 @@ import lombok.Builder;
  * JLMap Exception which is thrown when changing the map before it gets ready!
  * Leaflet map gets fully ready after the {{@link javafx.concurrent.Worker.State}} of {{@link javafx.scene.web.WebEngine}}
  * changes to {{@link javafx.concurrent.Worker.State#SUCCEEDED}}
- * by: Mehdi Akbarian Rastaghi (@makbn)
+ * @author Mehdi Akbarian Rastaghi (@makbn)
  */
 public class JLMapNotReadyException extends JLException{
+    private static final String MAP_IS_NOT_READY_YET = "Map is not ready yet!";
 
     @Builder
     public JLMapNotReadyException() {
-        super("map is not ready yet!");
+        super(MAP_IS_NOT_READY_YET);
     }
 }
