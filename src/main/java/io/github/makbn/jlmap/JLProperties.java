6a8921928106b686f48bce42f4e780ef0c170ddf
“makbn” <Mehdi74akbarian@gmail.com>
2023-09-09 03:32:34 -0600

    enhance JLProperties with MapType constants to simplify map type selection and configuration, improving code readability and maintainability. this change introduces a set of MapType constants, including popular map styles like OpenStreetMap, Stadia, Thunderforest, and Jawg, along with their corresponding parameters. by providing these constants, developers can easily specify their desired map type when configuring a JLMapView, reducing the risk of typographical errors and making the code more self-explanatory. Additionally, the introduction of default values for map types streamlines map setup, making it more convenient for users to get started with JLMapView.

--- src/main/java/io/github/makbn/jlmap/JLProperties.java
+++ src/main/java/io/github/makbn/jlmap/JLProperties.java
@@ -1,35 +1,54 @@
 package io.github.makbn.jlmap;
 
+import io.github.makbn.jlmap.model.JLMapOption;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Collections;
+import java.util.Set;
+
 /**
- * by: Mehdi Akbarian Rastaghi (@makbn)
+ * @author Mehdi Akbarian Rastaghi (@makbn)
  */
 public class JLProperties {
-
-    public final static int INIT_MIN_WIDTH = 1024;
-    public final static int INIT_MIN_HEIGHT = 576;
-    public final static int NORMAL_MARGIN = 0;
-    public final static int EARTH_RADIUS = 6371;
-    public final static int DEFAULT_CIRCLE_RADIUS = 200;
-    public final static int DEFAULT_CIRCLE_MARKER_RADIUS = 10;
-
-    public final static int INIT_MIN_WIDTH_STAGE = INIT_MIN_WIDTH;
-    public final static int INIT_MIN_HEIGHT_STAGE = INIT_MIN_HEIGHT;
+    public static final int INIT_MIN_WIDTH = 1024;
+    public static final int INIT_MIN_HEIGHT = 576;
+    public static final int EARTH_RADIUS = 6367;
+    public static final int DEFAULT_CIRCLE_RADIUS = 200;
+    public static final int DEFAULT_CIRCLE_MARKER_RADIUS = 10;
+    public static final int INIT_MIN_WIDTH_STAGE = INIT_MIN_WIDTH;
+    public static final int INIT_MIN_HEIGHT_STAGE = INIT_MIN_HEIGHT;
     public static final double START_ANIMATION_RADIUS = 10;
 
+    @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+    public record MapType(String name, Set<JLMapOption.Parameter> parameters) {
 
-    public enum MapType{
-        LIGHT("mapbox/light-v10"), DARK("mapbox/dark-v10"), OUTDOOR("mapbox/outdoors-v11"),
-        SATELLITE("mapbox/satellite-v9"), SATELLITE_STREET("mapbox/satellite-streets-v11"),
-        STREET("mapbox/streets-v11");
-
-        private final String value;
-
-        MapType(String value) {
-            this.value = value;
+        public MapType(String name) {
+            this(name, Collections.emptySet());
         }
 
-        public String getValue() {
-            return value;
+        public static final MapType OSM_MAPNIK = new MapType("OpenStreetMap.Mapnik");
+        public static final MapType OSM_HOT = new MapType("OpenStreetMap.HOT");
+        public static final MapType OPEN_TOPO = new MapType("OpenTopoMap");
+        public static final MapType STADIA_ALIDADE_SMOOTH_LIGHT = new MapType("Stadia.AlidadeSmooth");
+        public static final MapType STADIA_ALIDADE_SMOOTH_DARK = new MapType("Stadia.AlidadeSmoothDark");
+        public static final MapType STADIA_OSM_BRIGHT = new MapType("Stadia.OSMBright");
+        public static final MapType STADIA_WATERCOLOR = new MapType("Stadia.StamenWatercolor");
+        public static final MapType STADIA_STAMEN_TONER_LITE = new MapType("Stadia.StamenTonerLite");
+        public static final MapType STADIA_STAMEN_TERRAIN = new MapType("Stadia.StamenTerrain");
+
+        public static final MapType OPNV_KARTE = new MapType("OPNVKarte");
+        public static final MapType THUNDERFOREST_CYCLE = new MapType("Thunderforest.OpenCycleMap");
+        public static final MapType THUNDERFOREST_TRANSPORT = new MapType("Thunderforest.Transport");
+        public static final MapType THUNDERFOREST_SPINAL = new MapType("Thunderforest.SpinalMap");
+
+        public static final MapType JAWG_TERRAIN = new MapType("Jawg.Terrain");
+        public static final MapType JAWG_LIGHT = new MapType("Jawg.Light");
+        public static final MapType JAWG_DARK = new MapType("Jawg.Dark");
+        public static final MapType JAWG_MATRIX = new MapType("Jawg.Matrix");
+
+        public static MapType getDefault() {
+            return OSM_MAPNIK;
         }
     }
 }
