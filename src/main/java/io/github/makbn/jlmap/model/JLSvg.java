5ba9bae1495b2ef58a891d5cb048886443ae311b
Matt <mehdi.rastaghi@aimsio.com>
2023-09-10 21:19:05 -0600

    improving code quality. improving documentation.

--- src/main/java/io/github/makbn/jlmap/model/JLSvg.java
+++ src/main/java/io/github/makbn/jlmap/model/JLSvg.java
@@ -1,8 +1,12 @@
 package io.github.makbn.jlmap.model;
 
+import jdk.jshell.spi.ExecutionControl;
+
 /**
  * @author Mehdi Akbarian Rastaghi (@makbn)
  */
-class JLSvg {
-
+public class JLSvg {
+    public JLSvg() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("SVG is not implemented!");
+    }
 }
